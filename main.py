4e85456c8ac1ab693a5f72cb840cf38ba9fc70b4
akarshsinghh <akarshsinghh@gmail.com>
2024-08-19 23:36:09 -0400

    test for M and U

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dlClassifier import logger
 from dlClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from dlClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from dlClassifier.pipeline.stage03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +20,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
